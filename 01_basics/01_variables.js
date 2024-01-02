3f75a7a2f5c5f76bc40f69be49c994ed1554f9c2
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2024-01-02 13:49:23 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accId=14453
+let accEmail="goku@google.com"
+var accPass="12345"
+accntCity="bengaluru"
+let accState 
+/*prefer not to use var because of block scope
+ and functional scope */
+console.table([accId,accEmail,accPass,accntCity,accState])
