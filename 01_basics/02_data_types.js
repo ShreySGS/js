69c8ba338a90a4ef8348926c4b379612d388f318
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2024-01-02 13:48:04 +0000

    data_type

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,17 @@
+"use strict" // treats all JS code as newer version
+
+//alert( 3 + 3 ) // we are using nodejs, not browser
+
+// number -> 2^53
+// BigInt
+// String -> ""
+// boolean -> T/F
+// null -> standalone value
+// undefined
+// symbol -> unique
+
+// object
+
+console.log(typeof null); // -> object
+// -> undefined
+
