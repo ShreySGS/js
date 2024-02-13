b6445f7f83ae41914c421971c9e861f7eb5305a4
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2024-02-13 10:57:10 +0000

    arrays part 1 done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// array
+// not fixed size
+const arr = [0, 1, 2, 3, 4, 5]
+
+const myArr = new Array(0, 1, 2, 3, 4, 5)
+// console.log(myArr[1]);
+// console.log(arr[0]);
+
+// Array methods
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9) adds ele to the start 
+// myArr.shift() removes 1st ele
+// console.log(myArr.includes(9))//False
+// console.log(myArr.index(9))//-1
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr)
+
+// slice, splice
+
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1,3);(pos, pos)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1,4)(pos, range)
+
+// console.log(myn2);
+// console.log("C ", myArr);
