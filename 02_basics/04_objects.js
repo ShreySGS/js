b39d554787d7f336cb8a943349fe6a11953ffd35
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2024-02-15 07:30:43 +0000

    objects part2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "m@g.com",
+    fullname: {
+        userfullname: {
+            firstname: "Son",
+            lastname: "Goku"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+const obj4 = Object.assign({}, obj1, obj2, obj3)//(target->{}, source)
+// console.log(obj4);
+
+const obj5 = {...obj1, ...obj2, ...obj3}
+// console.log(obj5);
+
+// database obj come in array
+const users = [
+    {
+        id: 1,
+        email: "g@gamil.com"
+    },
+    {
+        id: 2,
+        email: "h@gamil.com"
+    },
+    {
+        id: 3,
+        email: "i@gamil.com"
+    }
+]
+
+// console.log(users[0].email); 0 indexing
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));// returns array with objkeys
+// console.log(Object.values(tinderUser));// returns array with objvalues
+// console.log(Object.entries(tinderUser));// returns key value paired arrays
+
+console.log(tinderUser.hasOwnProperty('isLoggeIn'));// returns bool
